b5faca1cfdcea98e0457b0ce0432e930259488f5
naaaani <nandi.zalka@gmail.com>
2023-11-17 12:26:46 +0100

    Start Over

--- src/main/java/com/codecool/solarwatch/controller/SunTimesController.java
+++
@@ -1,7 +0,0 @@
-package com.codecool.solarwatch.controller;
-
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-public class SunTimesController {
-}
--- src/main/java/com/codecool/solarwatch/model/SunTimesModel.java
+++
@@ -1,6 +0,0 @@
-package com.codecool.solarwatch.model;
-
-import java.time.LocalTime;
-
-public record SunTimesModel(LocalTime sunrise, LocalTime sunset) {
-}
